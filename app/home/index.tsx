74fc8cbd6d1669c56c69d5daf3e3663fbc9473be
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-01-04 00:05:36 -0400

    carousel options and image added

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 import { useMovies } from '@/presentation/hooks/useMovies'
-import  MainSlideShow  from '@/presentation/components/MainSlideShow'
+import  MainSlideShow  from '@/presentation/components/Movies/MainSlideShow'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
@@ -21,10 +21,9 @@ const HomeScreen = () => {
 
   return (
     <View className='mt-2' style={{paddingTop: safeArea.top}}>
-      <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
+      <Text className='text-3xl font-bold px-4 mb-2'>Movies App</Text>
       {/* Carousel of Movies */}
       <MainSlideShow movies={ nowPlayingQuery.data ?? []} />
-      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
     </View>
   )
 }
--- presentation/components/Movies/MainSlideShow.tsx
+++ presentation/components/Movies/MainSlideShow.tsx
@@ -2,6 +2,7 @@ import { Movie } from '@/core/infrastructure/interfaces/movie-interface';
 import { useRef } from 'react';
 import { Dimensions, Text, useWindowDimensions, View } from 'react-native';
 import Carousel, {ICarouselInstance} from 'react-native-reanimated-carousel';
+import MoviePoster from './MoviePoster';
 
 interface Props {
   movies: Movie[];
@@ -17,11 +18,11 @@ const MainSlideShow = ({movies}: Props) => {
     <View className='h-[250px] w-full'>
       <Carousel ref={ref} 
                 data={movies} 
-                renderItem={({item}) => <Text>{item.title}</Text>}
+                renderItem={({item}) => (<MoviePoster id={item.id} poster={item.poster} smallPoster={false}/>)}
                 width={200}
                 height={350}
                 style={{width: width, height: 350, justifyContent:'center', alignItems: 'center'}}
-                mode='parallax'
+                // mode='parallax'
                 modeConfig={{
                   parallaxScrollingScale:0.9,
                   parallaxScrollingOffset: 50
