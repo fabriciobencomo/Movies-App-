64b1751ffc35b95f6ef8918c46f7ca334f6040c4
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-01-04 11:45:19 -0400

    determine the end of horizontal scroll

--- presentation/components/Movies/MovieHorizontal.tsx
+++ presentation/components/Movies/MovieHorizontal.tsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
+import React, { useRef } from 'react'
 import { Movie } from '@/core/infrastructure/interfaces/movie-interface'
 import MoviePoster from './MoviePoster';
 
@@ -7,13 +7,33 @@ interface Props {
   title?: string,
   movies: Movie[];
   className?: string;
+  loadNextPage?: () => void
 }
 
-const MovieHorizontal = ({movies, title, className}: Props) => {
+const MovieHorizontal = ({movies, title, className, loadNextPage}: Props) => {
+
+  const isLoading = useRef(false);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if(isLoading.current) return
+
+    const {contentOffset, layoutMeasurement, contentSize} = event.nativeEvent
+
+    const isEndReach = (contentOffset.x + layoutMeasurement.width + 600) >=  contentSize.width
+
+    if(!isEndReach) return
+
+    isLoading.current = true;
+
+    // TODO: 
+    console.log('cargar Nuevas Peliculas')
+  }
+
   return (
     <View className={`{${className}`}>
       <Text className='text-3xl font-bold px-4 mb-4 mt-2'>{title}</Text>
-      <FlatList showsHorizontalScrollIndicator={false} horizontal data={movies} renderItem={({item}) => <MoviePoster id={item.id} poster={item.poster} smallPoster/>} />
+      <FlatList showsHorizontalScrollIndicator={false} horizontal data={movies} renderItem={({item}) => <MoviePoster id={item.id} poster={item.poster} smallPoster/>} onScroll={onScroll}/>
+
     </View>
   )
 }
