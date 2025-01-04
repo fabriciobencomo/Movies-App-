2921f62ff986bd48c2cd95e9b1f2bc99665a33b2
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-01-03 22:28:12 -0400

    infracstructure create for mapping the data from api movieDB

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import "../global.css";
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 
 const RootLayout = () => {
+
+  nowPlayingAction();
+  
   return (
     <View>
       <Text>RootLayout</Text>
